b1e9e41a7cacd208b7f18d61fd8c809299782609
seungeun <tmddms1002@gmail.com>
2024-12-09 21:19:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 45.42 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,34 +1,33 @@
-import java.util.*;
+import java.util.*; 
 
 class Solution {
     public int solution(String s) {
-        HashMap<Character, Character> map = new HashMap<>();
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
+        HashMap<Character, Character> map = new HashMap<>();        
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
         
-        int n = s.length();
+        int n = s.length(); // 원래 길이 저장
         s += s;
         int answer = 0;
         
-        A:for (int i = 0; i < n; i++) {
-            ArrayDeque<Character> stack = new ArrayDeque<>();
-            
-            for (int j=i; j < i+n; j++) {
-                char c = s.charAt(j);
-            
-                if (!map.containsKey(c)) {
-                    stack.push(c);
+        A : for (int i =0; i<n; i++) {
+            Stack<Character> stack = new Stack<>();
+            for (int j=i; j<n+i; j++) {
+                if (!map.containsKey(s.charAt(j))){
+                    stack.push(s.charAt(j));
                 }
                 else {
-                    if(stack.isEmpty() || !stack.pop().equals(map.get(c)))
-                        continue A;
+                   if (stack.isEmpty() || !stack.pop().equals(map.get(s.charAt(j))))
+                       continue A;
                 }
+                    
             }
-        
+            
             if (stack.isEmpty())
                 answer++;
         }
+        
         return answer;
     }
 }
