034bd424a908314583d4f989eec0ce9c70abed63
seungeun <tmddms1002@gmail.com>
2025-07-01 20:13:49 +0900

    [level 1] Title: 모의고사, Time: 5.60 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -5,29 +5,24 @@ class Solution {
         int[][] answer = {{1,2,3,4,5},
                           {2,1,2,3,2,4,2,5},
                           {3,3,1,1,2,2,4,4,5,5}};
+        int[] score = new int[3];
+        for (int i=0; i<answers.length; i++){
+            for (int j=0; j<answer.length; j++){
+                if (answers[i]==answer[j][i%answer[j].length])
+                    score[j]++;
+            }
+        }
         
-        int[] solution = {0,0,0};
+        
+        int maxValue = Arrays.stream(score).max().getAsInt();
         ArrayList<Integer> list = new ArrayList<>();
-        
-        for (int i = 0; i<answers.length; i++) {
-            if (answers[i] == answer[0][i%5]) {
-                solution[0]++;
-            }
-            if (answers[i] == answer[1][i%8]) {
-                solution[1]++;
-            }
-            if (answers[i] == answer[2][i%10]) {
-                solution[2]++;
-            }
-        }
-        
-        int maxValue = Arrays.stream(solution).max().getAsInt();
-        for (int i =0; i<3; i++) {
-            if (solution[i] == maxValue) {
+        for (int i=0; i<score.length; i++){
+            if (maxValue == score[i])
                 list.add(i+1);
-            }
         }
         
-        return list.stream().sorted().mapToInt(Integer::intValue).toArray();
+        int[] result = list.stream().mapToInt(Integer::intValue).toArray(); 
+        
+        return result;
     }
 }
