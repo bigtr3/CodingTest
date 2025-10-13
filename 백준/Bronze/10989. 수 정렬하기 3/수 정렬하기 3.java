acac169abc32a11cd02cd240a57e7da3282cecfe
seungeun <tmddms1002@gmail.com>
2025-10-13 22:53:15 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1644 ms, Memory: 301052 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] count = new int[10001];
+        for (int i=0; i<N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            count[num]++;
+        }
+
+        for (int i=1; i<10001; i++) {
+            while (count[i]-- > 0) {
+                bw.write(i+"\n");
+            }
+        }
+        bw.flush();
+    }
+}
