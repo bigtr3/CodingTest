990c82f71cc2aaa2abe894dd7dcb4bae885cff82
seungeun <tmddms1002@gmail.com>
2025-11-15 22:46:37 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=N/5; i>=0; i--) {
+            if ((N - i*5) % 3 == 0) {
+                System.out.print(i + (N - i*5) / 3);
+                break;
+            }
+
+            if (i == 0) {
+                System.out.print(-1);
+            }
+        }
+    }
+}
