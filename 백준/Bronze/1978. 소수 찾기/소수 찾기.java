088bfa725003fcb1ee95145572fbeccb4ef1c9c2
seungeun <tmddms1002@gmail.com>
2025-12-04 23:08:17 +0900

    [Bronze II] Title: 소수 찾기, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,27 +1,27 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int count =0;
-        for (int i=0; i<N; i++) {
-            int  num = sc.nextInt();
-            if (num < 2) {
-                continue;
-            }
+import java.util.*;
+import java.io.*;
 
-            if (num == 2) {
-                count++;
-                continue;
-            }
-            for (int j=2; j<num; j++) {
-                if (num % j == 0){
-                    break;
-                } else if (j==num-1){
-                    count++;
-                }
-            }
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String str = br.readLine();
+        int count = 0;
+        for (int i=0; i<N; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            if (isPrime(a)) count++;
         }
-        System.out.println(count);
+        System.out.print(count);
+    }
+
+    static boolean isPrime(int a){
+        if (a < 2) return false;
+        for (int i=2; i<=Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
+            } 
+        }
+        return true;
     }
 }
